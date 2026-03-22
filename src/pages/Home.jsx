af47cf273781cb3decfeafd857a4b97555b136a4
Shivansh Puri <shiwash122@gmail.com>
2026-03-22 20:59:44 +0530

    Add:Category component to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,22 @@
-import {useState, useEffect } from "react"
-import SearchBar from "../components/Searchbar"
-
+import { useState, useEffect } from "react";
+import SearchBar from "../components/Searchbar";
+import CategoryGrid from "../components/CategoryGrid";
 
 function Home() {
-    return (
-        <div>
-            {/* Hero Section */}
-            <section className="hero">
-                <div className="hero-content">
-                    <h2>Shop Everything</h2>
-                    <h2>Delivered Fast</h2>
-                    <p>Discover thousands of products at best prices.</p>
-                    <SearchBar />
-                </div>
-            </section>
+  return (
+    <div>
+      {/* Hero Section */}
+      <section className="hero">
+        <div className="hero-content">
+          <h2>Shop Everything</h2>
+          <h2>Delivered Fast</h2>
+          <p>Discover thousands of products at best prices.</p>
+          <SearchBar />
         </div>
-    )
+      </section>
+      <CategoryGrid />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
