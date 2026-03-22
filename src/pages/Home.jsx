04996d11e938662fbeac22a74dd362888ccb934c
Shivansh Puri <shiwash122@gmail.com>
2026-03-22 13:02:45 +0530

    Add:Hero section and Search bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
+import {useState, useEffect } from "react"
+import SearchBar from "../components/Searchbar"
+
+
 function Home() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  )
+    return (
+        <div>
+            {/* Hero Section */}
+            <section className="hero">
+                <div className="hero-content">
+                    <h2>Shop Everything</h2>
+                    <h2>Delivered Fast</h2>
+                    <p>Discover thousands of products at best prices.</p>
+                    <SearchBar />
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Home
